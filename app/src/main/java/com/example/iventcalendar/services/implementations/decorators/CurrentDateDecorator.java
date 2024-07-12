6414961988dfea1053106b8ded4c73d20b40c715
21MokseM12 <maksim.buynov.00@mail.ru>
2024-07-13 01:45:10 +0300

    code review; refactoring; change directory architecture;

--- app/src/main/java/com/example/iventcalendar/services/implementations/decorators/CurrentDateDecorator.java
+++ app/src/main/java/com/example/iventcalendar/services/implementations/decorators/CurrentDateDecorator.java
@@ -1,32 +1,29 @@
-package com.example.iventcalendar.service.decorators;
+package com.example.iventcalendar.services.implementations.decorators;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 
-public class ActiveEventDecorator implements DayViewDecorator {
-    private final SharedPreferences flags;
+public class CurrentDateDecorator implements DayViewDecorator {
+    private CalendarDay date;
     private final Drawable drawable;
-
     @SuppressLint("UseCompatLoadingForDrawables")
-    public ActiveEventDecorator(Context context, SharedPreferences flags, int resId) {
-        this.flags = flags;
-        this.drawable = context.getDrawable(resId);
+    public CurrentDateDecorator(Context context, int drawableResId) {
+        date = null;
+        drawable = context.getDrawable(drawableResId);
     }
     @Override
     public boolean shouldDecorate(CalendarDay day) {
-        String date = String.valueOf(day.getDay()) + (day.getMonth()+1) + day.getYear();
-        return flags.contains(date);
+        return day.equals(date);
     }
 
     @Override
     public void decorate(DayViewFacade view) {
-        view.setBackgroundDrawable(drawable);
+        view.setSelectionDrawable(drawable);
     }
-
+    public void setDate(CalendarDay day) {date = day;}
 }
